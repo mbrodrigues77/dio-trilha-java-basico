e295a7d5a1614c34ba85f5dd0047b161fd8bc2ab
mbrodrigues77 <mbrodrigues77@gmail.com>
2024-06-05 11:29:54 -0300

    Inclusão dos exemplos sobre tipos de variáveis.

--- tipos-variaveis/src/TiposVaraveis.java
+++ tipos-variaveis/src/TiposVaraveis.java
@@ -1,5 +1,18 @@
 public class TiposVaraveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        /*Estudar Variáveis:
+         * - Tipos primitivos
+         * - Classe String, que representa texto na aplicação
+         */
+
+         String meuNome = "Marcelo Borges";
+
+         double salarioMinimo  = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
     }
 }
